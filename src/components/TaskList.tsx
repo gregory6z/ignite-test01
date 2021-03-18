cbb4753aa125324418f01cd4457795cc75e06eb3
gregory6z <gregoryrag@gmail.com>
2021-03-18 02:04:37 +0100

    testcompleted

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.z
+    if (!newTaskTitle) return 
+    const newTask = {
+      id:Math.random(),
+      title:newTaskTitle,
+      isComplete:false
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ?{
+      ...task,
+      isComplete: !task.isComplete
+    }: task)
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filterTask = tasks.filter(task => task.id !== id )
+    setTasks(filterTask)
   }
 
   return (
